95764ee0a4976df62e1bbefc185a80fa12981146
DaniloPath <danilo.castro.engcivil@gmail.com>
2020-07-07 20:01:46 -0300

    Verificando a existência de notas e adicionado-as.

--- frontend/src/api/apiService.js
+++ frontend/src/api/apiService.js
@@ -37,8 +37,56 @@ async function getAllGrades(){
             isDeleted: false
         }
     })
+    //Forma um conjunto de dados para se ter dados únicos
+    let allStudents = new Set()
+    grades.forEach(grade => allStudents.add(grade.student))
+    //Transforma em array
+    allStudents=Array.from(allStudents)
 
-    return grades
+    let allSubjects = new Set()
+    grades.forEach(grade => allSubjects.add(grade.subject))
+    allSubjects=Array.from(allSubjects)
+    
+    let allTypes = new Set()
+    grades.forEach(grade => allTypes.add(grade.type))
+    allTypes=Array.from(allTypes)
+
+    const allCombinations = []
+    allStudents.forEach(student=>{
+        allSubjects.forEach(subject=>{
+            allTypes.forEach(type=>{
+                allCombinations.push({
+                    student,
+                    subject,
+                    type,
+                })
+            })
+        })
+    })
+
+    allCombinations.forEach(({student, subject, type})=>{
+        const hasItem = grades.find((grade)=>{
+            grade.subject = subject && grade.student === student && grade.type === type
+        })
+
+        if(!hasItem){
+            //adicionando um item que não existe no back-end
+            grades.push({
+                id: grades.length+1,
+                student,
+                studentLowerCase: student.toLowerCase(),
+                subject,
+                subjectLowerCase: subject.toLowerCase(),
+                type,
+                typeLowerCase: type.toLowerCase(),
+                value: 0,
+                isDeleted: true,
+            })
+        }
+    })
+
+
+    return allCombinations
 }
 
 export {getAllGrades}
