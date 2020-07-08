f46dfe72e2c3715dbc2133f34696670c66cbb162
DaniloPath <danilo.castro.engcivil@gmail.com>
2020-07-07 23:46:22 -0300

    Criado state no APP

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import * as api from './api/apiService'
 
 
 export default function App() {
+  const [allGrades, setAllGrades] = useState([])
+  const [selectedGrade, setSelectedGrade] = useState({})
+  const [isModalOpen, setIsModalOpen] = useState(false)
 
-  const testApi = async () => {
-    const result=await api.getAllGrades()
-    console.log(result)
-  }
-  testApi()
 
-  return <h1>React Grades</h1>
+
+  return (
+    <div>
+      <h1>Controle de Notas</h1>
+
+      {allGrades.length > 0 && <p>Notas disponíveis</p> }
+      {allGrades.length == 0 && <p>Carregando notas...</p> }
+    </div>
+  )
   
 }
