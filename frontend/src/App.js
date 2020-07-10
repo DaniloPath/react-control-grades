d15b6f2dee8be4ac68dde494954613d3cade64b9
DaniloPath <danilo.castro.engcivil@gmail.com>
2020-07-10 02:13:23 -0300

    Adicionando o total das notas e os estilizando.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -35,6 +35,7 @@ export default function App() {
     console.log('handlePersist')
   }
   
+  
 
   return (
     <div>
--- frontend/src/components/GradesControl.js
+++ frontend/src/components/GradesControl.js
@@ -1,85 +1,125 @@
-import React from 'react'
-import Action from './Action'
+import React from 'react';
+import Action from './Action';
 
-export default function GradesControl({grades, onDelete, onPersist}) {
-   const tableGrades = []
+export default function GradesControl({ grades, onDelete, onPersist }) {
+  const tableGrades = [];
 
-   let currentStudent = grades[0].student
-   let currentSubject = grades[0].subject
-   let currentGrades = []
-   let id = 1
+  let currentStudent = grades[0].student;
+  let currentSubject = grades[0].subject;
+  let currentGrades = [];
+  let id = 1;
 
-   grades.forEach(grade => {
-      if (grade.subject !== currentSubject) {
-         tableGrades.push({
-            id: id++,
-            student: currentStudent,
-            subject: currentSubject,
-            grades: currentGrades,
-         })
+  grades.forEach((grade) => {
+    if (grade.subject !== currentSubject) {
+      tableGrades.push({
+        id: id++,
+        student: currentStudent,
+        subject: currentSubject,
+        grades: currentGrades,
+      });
 
-         currentSubject = grade.subject
-         currentGrades = []
-      }
+      currentSubject = grade.subject;
+      currentGrades = [];
+    }
 
-      if (grade.student !== currentStudent){
-         currentStudent = grade.student
-      }
+    if (grade.student !== currentStudent) {
+      currentStudent = grade.student;
+    }
 
-      currentGrades.push(grade)
-   })
+    currentGrades.push(grade);
+  });
 
-   tableGrades.push({
-      id: id++,
-      student: currentStudent,
-      subject: currentSubject,
-      grades: currentGrades,
-   })
+  tableGrades.push({
+    id: id++,
+    student: currentStudent,
+    subject: currentSubject,
+    grades: currentGrades,
+  });
 
-   const handleActionClick = (id, type) =>{
-      console.log(id)
-      console.log(type)
+  const handleActionClick = (id, type) => {
+    console.log(id);
+    console.log(type);
+  };
 
-   }
-   
-   return (
-      <div className="container">
-         {tableGrades.map(({id, grades}) => {
-            return (
-               <table className="striped container" key={id}>
-                  <thead>
-                     <tr>
-                        <th style={{width: "20%"}}>Aluno</th>
-                        <th style={{width: "20%"}}>Disciplina</th>
-                        <th style={{width: "20%"}}>Avaliação</th>
-                        <th style={{width: "20%"}}>Nota</th>
-                        <th style={{width: "20%"}}>Ações</th>                                      
-                     </tr>
-                  </thead>
-                  <tbody>
-                     {grades.map(({id, subject, student, type, value, isDeleted})=>{
-                        return (
-                           <tr key={id}>
-                              <td>{student}</td>
-                              <td>{subject}</td>
-                              <td>{type}</td>
-                              <td>{isDeleted ? '-': value}</td>
-                              <td>
-                                 <div>
-                                    <Action id={id} onActionClick={handleActionClick} type={isDeleted ? "add": "edit"}/>
-                                    {!isDeleted && <Action id={id} onActionClick={handleActionClick} type="delete" />}
-                                 </div>
-                              </td>                              
-                           </tr>
-                        )
-                     })}
-                  </tbody>
-                  <tfoot>
-                  </tfoot>
-               </table>
-                     )
-                  })}
-      </div>
-      
-   )
+  return (
+    <div className="container">
+      {tableGrades.map(({ id, grades }) => {
+        const finalGrade = grades.reduce((acc, curr) => acc + curr.value, 0);
+        const gradeStyle =
+          finalGrade >= 70 ? styles.goodGrade : styles.badGrade;
+
+        return (
+          <table style={styles.table} className="striped container" key={id}>
+            <thead>
+              <tr>
+                <th style={{ width: '20%' }}>Aluno</th>
+                <th style={{ width: '20%' }}>Disciplina</th>
+                <th style={{ width: '20%' }}>Avaliação</th>
+                <th style={{ width: '20%' }}>Nota</th>
+                <th style={{ width: '20%' }}>Ações</th>
+              </tr>
+            </thead>
+            <tbody>
+              {grades.map(
+                ({ id, subject, student, type, value, isDeleted }) => {
+                  return (
+                    <tr key={id}>
+                      <td>{student}</td>
+                      <td>{subject}</td>
+                      <td>{type}</td>
+                      <td>{isDeleted ? '-' : value}</td>
+                      <td>
+                        <div>
+                          <Action
+                            id={id}
+                            onActionClick={handleActionClick}
+                            type={isDeleted ? 'add' : 'edit'}
+                          />
+                          {!isDeleted && (
+                            <Action
+                              id={id}
+                              onActionClick={handleActionClick}
+                              type="delete"
+                            />
+                          )}
+                        </div>
+                      </td>
+                    </tr>
+                  );
+                }
+              )}
+            </tbody>
+            <tfoot>
+              <tr>
+                <td>&nbsp;</td>
+                <td>&nbsp;</td>
+                <td>&nbsp;</td>
+                <td style={{ textAlign: 'right' }}>
+                  <strong>Total: </strong>{' '}
+                </td>
+                <td>
+                  <span style={gradeStyle}>{finalGrade}</span>
+                </td>
+              </tr>
+            </tfoot>
+          </table>
+        );
+      })}
+    </div>
+  );
 }
+
+const styles = {
+  goodGrade: {
+    fontWeight: 'bold',
+    color: 'green',
+  },
+  badGrade: {
+    fontWeight: 'bold',
+    color: 'red',
+  },
+  table: {
+    margin: '20px',
+    padding: '10px',
+  },
+};
