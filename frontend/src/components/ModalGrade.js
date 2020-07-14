ee7d618913f24122f2152617b67b22be3afa7b0f
DaniloPath <danilo.castro.engcivil@gmail.com>
2020-07-13 22:38:04 -0300

    Faz as states

--- frontend/src/components/ModalGrade.js
+++ frontend/src/components/ModalGrade.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import Modal from 'react-modal';
+import { useState } from 'react';
 
 Modal.setAppElement('#root');
 
 export default function ModalGrade({ onSave, onClose, selectedGrade }) {
+  const { gradeValue, setGradeValue } = useState(selectedGrade.value);
+  //
+  const { gradeValidation, setGradeValidation } = useState({});
+  const { erroMessage, setErroMessage } = useState('');
   return (
     <div>
       <Modal isOpen={true} />
