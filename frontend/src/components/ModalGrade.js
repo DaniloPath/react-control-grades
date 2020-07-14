a0e233f7de770a16ca6f1f24abeaf73a343140a6
DaniloPath <danilo.castro.engcivil@gmail.com>
2020-07-13 22:57:54 -0300

    Implementa validação de notas

--- frontend/src/components/ModalGrade.js
+++ frontend/src/components/ModalGrade.js
@@ -1,14 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Modal from 'react-modal';
-import { useState } from 'react';
+
+import * as api from '../api/apiService';
 
 Modal.setAppElement('#root');
 
 export default function ModalGrade({ onSave, onClose, selectedGrade }) {
   const { gradeValue, setGradeValue } = useState(selectedGrade.value);
-  //
   const { gradeValidation, setGradeValidation } = useState({});
-  const { erroMessage, setErroMessage } = useState('');
+  const { errorMessage, setErrorMessage } = useState('');
+
+  useEffect(() => {
+    const validation = api.getValidationFromGradeType(type);
+    setGradeValidation(validation);
+  }, [type]);
+
+  useEffect(() => {
+    const { minValue, maxValue } = gradeValidation;
+    if (gradeValidation.value < minValue || gradeValue > maxValue) {
+      setErrorMessage(
+        `O valor da nota deve ser entre ${minValue} e ${maxValue}`
+      );
+      return;
+    }
+    setErrorMessage('');
+  }, [gradeValue, gradeValidation]);
+
   return (
     <div>
       <Modal isOpen={true} />
